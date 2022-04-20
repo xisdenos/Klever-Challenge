2126caa94efab587dbadb8d44bc9efac2e7ddd5f
xisdenos <lucaspinto.andrade@gmail.com>
2022-04-20 16:43:36 -0300

    Initial Commit

---
+++ Klever Challenge/Klever_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Klever_ChallengeApp.swift
+//  Klever Challenge
+//
+//  Created by Lucas Pinto on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Klever_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Klever ChallengeUITests/Klever_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Klever_ChallengeUITestsLaunchTests.swift
+//  Klever ChallengeUITests
+//
+//  Created by Lucas Pinto on 20/04/22.
+//
+
+import XCTest
+
+class Klever_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
